0f7302a3baa8333b05bf4198233cf81744d9327c
Tariq Lashley <tariqlash@gmail.com>
2024-04-05 23:03:00 -0600

    change hero margins

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ const Hero = () => {
     <div className="hero h-screen">
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-2xl">
-          <h1 className="mb-5 text-5xl font-extrabold playfair">At Home or at the Cabin</h1>
-          <h3 className='mb-5 font-bold'>Quality Painters for all your painting needs</h3>
+        <div className="max-w-2xl mb-32">
+          <h1 className="mb-8 text-5xl font-extrabold playfair">At Home or at the Cabin</h1>
+          <h3 className='mb-12 font-bold'>Quality Painters for all your painting needs</h3>
           <p className="mb-5 w-full font-semibold pb-5">JJJ Painting strives to deliver the highest-end results to our customers.
             We are meticulous perfectionists who do the job right the first time, while only using the
             top of the line products. With over 500 painting projects completed, the JJJ Painting team
